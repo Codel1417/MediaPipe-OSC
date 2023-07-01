c14505c6253ef1f9d5c53f1a503ff05bb1d6f8fc
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2023-07-01 19:41:10 -0400

    EXE Packaging changes

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # MediaPipe OSC
 
-This wrapps Google MediaPipe Face Landmark API for real time webcam based face and eye tracking. This is designed to be used with [VRCFT-MediaPipe](https://github.com/Codel1417/VRCFT-MediaPipe).
+This wraps Google MediaPipe Face Landmark API for real time webcam based face and eye tracking. This is designed to be used with [VRCFT-MediaPipe](https://github.com/Codel1417/VRCFT-MediaPipe).
 
+
+## Build
+Building an EXE
+
+```shell
+pyinstaller --noconfirm --onefile --add-data './face_landmarker.task;.'  .\mediaPipeFaceTracking.py
+```
--- mediaPipeFaceTracking.py
+++ mediaPipeFaceTracking.py
@@ -1,26 +1,32 @@
 import os
+import sys
+from os import path
 import cv2
 import mediapipe as mp
 from pythonosc import udp_client
+
+model_path = path.abspath(path.join(path.dirname(__file__), 'face_landmarker.task'))
+print(model_path)
 
 print('Starting OSC')
 OSCip = "127.0.0.1"
 OSCport = 8888  # VR Chat OSC port
 client = udp_client.SimpleUDPClient(OSCip, OSCport)
 
-# requires absolute path
-model_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), "./face_landmarker.task")
 BaseOptions = mp.tasks.BaseOptions
 FaceLandmarker = mp.tasks.vision.FaceLandmarker
 FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 FaceLandmarkerResult = mp.tasks.vision.FaceLandmarkerResult
 
+
 # Create a face landmarker instance with the live stream mode:
 def print_result(result: FaceLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     for category in result.face_blendshapes:
         for blendshape in category:
             client.send_message("/" + blendshape.category_name, blendshape.score)
+
+
 options = FaceLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
